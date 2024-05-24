0cad7c9752b2af4b4cba8d2de4c5841b7ff957e4
giosurmanidze <giorgisurmanidze917@gmail.com>
2024-05-24 12:10:54 +0400

    feat/protecd routes

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\TopicController;
 use Illuminate\Support\Facades\Route;
 
 
- 
-Route::get('/topics', [TopicController::class, 'index']);
-Route::get('/topics/{id}', [TopicController::class, 'show']);
-Route::post('/topics', [TopicController::class, 'store']);
-Route::patch('/topics/{topic}', [TopicController::class, 'update']);
+Route::group(['middleware' => ['api', 'auth:api']], function () {
+    Route::get('/topics', [TopicController::class, 'index']);
+    Route::get('/topics/{id}', [TopicController::class, 'show']);
+    Route::post('/topics', [TopicController::class, 'store']);
+    Route::patch('/topics/{topic}', [TopicController::class, 'update']);
+
+});
